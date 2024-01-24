04bc195c18cc2119e765015b81cc7c3808818566
Ed Jee <edjee96@gmail.com>
2024-01-24 16:30:40 -0600

    pretty printing

--- src/GrowthModels.jl
+++ src/GrowthModels.jl
@@ -3,6 +3,8 @@ module GrowthModels
     # Package dependencies
     using Plots
     using SparseArrays, LinearAlgebra
+    import Base: show
+    using Printf
 
     # exports
     ## util exports
@@ -29,7 +31,19 @@ module GrowthModels
     include("utils.jl")
     include("Skiba.jl")
 
+    function show(io::IO, m::SkibaModel)
+        print(io, "SkibaModel: γ = ", m.γ, ", α = ", m.α, ", ρ = ", m.ρ, ", δ = ", m.δ, ", A_H = ", m.A_H, ", A_L = ", m.A_L, ", κ = ", m.κ)
+    end
+
+function show(io::IO, h::HyperParams)
+    print(io, "HyperParams(N = ", h.N, ", dk = ", @sprintf("%.3g", h.dk), ", kmax = ", @sprintf("%.3g", h.kmax), ", kmin = ", @sprintf("%.3g", h.kmin), ")")
 end
+
+    function show(io::IO, v::Value)
+        print(io, "Value function with convergence status: ", v.convergence_status, ". Number of iterations: ", v.iter)
+    end
+end
+
 
 
 
