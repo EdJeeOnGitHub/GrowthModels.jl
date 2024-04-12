d2cee2f4e95345c1c7f12b37b75da4f29ef70f54
EdJeeOnGitHub <edjee96@gmail.com>
2024-04-12 10:21:53 -0500

    removed pow

--- src/Skiba.jl
+++ src/Skiba.jl
@@ -46,25 +46,25 @@ y_L(m::SkibaModel) = (k) -> m.A_L*k^m.α
 
 # Skiba production function
 @inline function skiba_production_function(k, α, A_H, A_L, κ)
-    max(A_H * pow(max(k - κ, 0), α), A_L * pow(k, α))
+    max.(A_H .* max.(k - κ, 0) .^ α, A_L .* (k.^α))
 end
 # derivative of skiba production function
 @inline function skiba_production_function_prime(k, α, A_H, A_L, κ)
     if k > κ
-        A_H * α * pow(k - κ, α - 1)
+        A_H * α * (k - κ) ^ (α - 1)
     else
-        A_L * α * pow(k, α - 1)
+        A_L * α * k ^ (α - 1)
     end
 end
 
 
-@inline production_function(::SkibaModel, k::Union{Real,Vector{<:Real}}, α::Real, A_H::Real, A_L::Real, κ::Real) = skiba_production_function.(k, α, A_H, A_L, κ)
-@inline production_function(::SkibaModel, k::Union{Real,Vector{<:Real}}, params::Vector) = skiba_production_function.(k, params[1], params[2], params[3], params[4])
-@inline production_function(m::SkibaModel, k::Union{Real,Vector{<:Real}}) = skiba_production_function.(k, m.α, m.A_H, m.A_L, m.κ)
+@inline production_function(::SkibaModel, k, α::T, A_H::T, A_L::T, κ::T) where {T <: Real} = skiba_production_function.(k, α, A_H, A_L, κ)
+@inline production_function(::SkibaModel, k, params::Vector) = skiba_production_function.(k, params[1], params[2], params[3], params[4])
+@inline production_function(m::SkibaModel, k) = skiba_production_function.(k, m.α, m.A_H, m.A_L, m.κ)
 
-@inline production_function_prime(::SkibaModel, k::Union{Real,Vector{<:Real}}, α::Real, A_H::Real, A_L::Real, κ::Real) = skiba_production_function_prime.(k, α, A_H, A_L, κ)
-@inline production_function_prime(::SkibaModel, k::Union{Real,Vector{<:Real}}, params::Vector) = skiba_production_function_prime.(k, params[1], params[2], params[3], params[4])
-@inline production_function_prime(m::SkibaModel, k::Union{Real,Vector{<:Real}}) = skiba_production_function_prime.(k, m.α, m.A_H, m.A_L, m.κ)
+@inline production_function_prime(::SkibaModel, k, α::T, A_H::T, A_L::T, κ::T) where {T <: Real} = skiba_production_function_prime.(k, α, A_H, A_L, κ)
+@inline production_function_prime(::SkibaModel, k, params::Vector) = skiba_production_function_prime.(k, params[1], params[2], params[3], params[4])
+@inline production_function_prime(m::SkibaModel, k) = skiba_production_function_prime.(k, m.α, m.A_H, m.A_L, m.κ)
 
 
 
