e217494d9779900d3616f6e6fc9768336e5a6f32
Ed Jee <edjee96@gmail.com>
2025-02-18 10:48:58 -0600

    forgot the model file..

---
+++ src/StochasticNPAbility.jl
@@ -0,0 +1,39 @@
+
+#### Utils for instantiating the model -----------------------------------------
+function StochasticNPAbilityModel(
+    stochasticprocess::Union{StochasticProcess,Nothing}; 
+     γ = 2.0, ρ = 0.05, δ = 0.05, f = (x, z, η) -> (x .^0.1) .* z .* η, η_mean = 1.0)
+    if isnothing(stochasticprocess)
+        stochasticprocess = OrnsteinUhlenbeckProcess(θ = -log(0.9), σ =  0.1)
+    end
+    StochasticNPAbilityModel(γ = γ, ρ = ρ, δ = δ, f = f, η_mean = η_mean, stochasticprocess = stochasticprocess)
+end
+
+function StochasticNPAbilityModel(;γ = 2.0, ρ = 0.05, δ = 0.05, f = (x, z, η) -> (x .^ 0.1) .* z .* η , η_mean = 1.0, θ = -log(0.9), σ = 0.1)
+    StochasticNPAbilityModel(γ, ρ, δ, f, η_mean, OrnsteinUhlenbeckProcess(θ = θ, σ = σ))
+end
+
+# let us pass param vecs
+function StochasticNPAbilityModel(γ, ρ, δ, f, η_mean, θ, σ)
+    StochasticNPAbilityModel(γ, ρ, δ, f, η_mean, OrnsteinUhlenbeckProcess(θ = θ, σ = σ))
+end
+
+
+#### Steady State + Star Helpers -----------------------------------------------
+# OrnsteinUhlenbeckProcess
+k_steady_state_hi(m::StochasticNPAbilityModel{T, S}) where {T <: Real, S <: OrnsteinUhlenbeckProcess}= error("Not implemented")
+k_steady_state_lo(m::StochasticNPAbilityModel{T, S}) where {T <: Real, S <: OrnsteinUhlenbeckProcess} = error("Not implemented")
+# PoissonProcess
+k_steady_state_hi(m::StochasticNPAbilityModel{T, S}) where {T <: Real, S <: PoissonProcess} = error("Not implemented")
+k_steady_state_lo(m::StochasticNPAbilityModel{T, S}) where {T <: Real, S <: PoissonProcess} = error("Not implemented")
+
+
+k_steady_state(m::StochasticNPAbilityModel) = [k_steady_state_lo(m), k_steady_state_hi(m)]
+k_star(m::StochasticNPAbilityModel) = error("Not implemented")
+
+
+
+
+
+#### Production Function -------------------------------------------------------
+production_function(m::StochasticNPAbilityModel{T, S}, k, z, η) where {T <: Real, S <: StochasticProcess} = m.f(k, z, η)
